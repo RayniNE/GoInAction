81f5bad3ce5fcb95a3c4bdbf60f1fb093e257875
RayniNE <timetopwm@gmail.com>
2021-08-12 00:08:27 -0400

    Added the remaining files of the chapter 2 project.

--- Chapter2/search/feed.go
+++ Chapter2/search/feed.go
@@ -7,12 +7,14 @@ import (
 
 const dataFile = "data/datajson"
 
+// Feed contains information we need to process a feed.
 type Feed struct {
 	Name string `json:"site"`
 	URI  string `json:"link"`
 	Tyoe string `json:"type"`
 }
 
+// RetrieveFeeds reads and unmarshals the feed data file.
 func RetrieveFeeds() ([]*Feed, error) {
 	// We open the file.
 	file, err := os.Open(dataFile)
@@ -20,11 +22,16 @@ func RetrieveFeeds() ([]*Feed, error) {
 		return nil, err
 	}
 
+	// Schedule the file to be closed once
+	// the function returns.
 	defer file.Close()
 
+	// Decode the file into a slice of pointers
+	// to Feed values.
 	var feeds []*Feed
 	err = json.NewDecoder(file).Decode(&feeds)
 
+	// We don't need to check for errors, the caller can do this.
 	return feeds, err
 
 }
